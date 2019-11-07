02a822ca8fad8b9529ee9b1f18dfaf827acfda6c
zak427zak <zak427zak@yandex.ru>
2019-11-07 16:52:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter.tix import *
 import datetime
 from datetime import timedelta
+from functools import partial
 import xlwt
 import random
 import math
@@ -55,7 +56,7 @@ def lower_in_morning(total_days):
 def randomOrder_key(element):
     return random()
 
-def run_calc():
+def run_generator(values_dict):
 
     start_date_f = start_date_value.get()
     start_date = start_date_f.split('.')
@@ -114,20 +115,25 @@ def run_calc():
         pass
 
 
-def run_generator():
+def run_generator(values_dict):
+    print('111')
+    ab = values_dict
+    bc = 111
     pass
 
-def collect_inserted_data(**kwargs):
-    pass
+def complex_label(label_first, label_second):
+    return label_first.cget('text') + label_second.cget('text')
 
-
-def add_value_to_dict(label,value,values_dict):
-    values_dict[label.cget('text')] = value.get()
+def get_data_to_generate(value,values_dict, *args):
+    label = ''
+    for a in args:
+        label += a.cget('text')
+    values_dict[label] = value.get()
 
 def create_and_configure_window(normal_font, normal_width, small_width, values_dict):
     start_window = Tk()
     start_window.title("Генератор АД, v1.1")
-    start_window.geometry('550x620')
+    start_window.geometry('550x700')
 
     lbl_main = Label(start_window, text="Заполните предложенные поля, затем нажмите кнопку внизу", font=normal_font)
     lbl_main.place(x=20, y=40)
@@ -139,7 +145,7 @@ def create_and_configure_window(normal_font, normal_width, small_width, values_d
     your_name_value.place(x=200, y=70)
     your_name_value.insert(0, 'Василий Иванов')
 
-    add_value_to_dict(your_name_label, your_name_value, values_dict)
+    get_data_to_generate(your_name_value, values_dict, your_name_label)
 
 
     lbl_periods = Label(start_window, text="1. Задайте периоды:", font=normal_font)
@@ -150,19 +156,19 @@ def create_and_configure_window(normal_font, normal_width, small_width, values_d
     start_date_value = Entry(start_window, width=15)
     start_date_value.place(x=200, y=100)
     start_date_value.insert(0, '01.05.2019')
-    add_value_to_dict(start_date_label, start_date_value, values_dict)
+    get_data_to_generate(start_date_value, values_dict, start_date_label)
 
     end_date_label = Label(start_window, text='Дата конца измерения:', width=normal_width)
     end_date_label.place(x=40, y=130)
     end_date_value = Entry(start_window, width=15)
     end_date_value.place(x=200, y=130)
     end_date_value.insert(0, '01.08.2019')
-    add_value_to_dict(end_date_label, end_date_value, values_dict)
+    get_data_to_generate(end_date_value, values_dict, end_date_label)
 
     lbl_periods = Label(start_window, text="2. Укажите границы АД:", font=normal_font)
     lbl_periods.place(x=40, y=170)
 
-    lbl_high_ad = Label(start_window, text="Для повышенного давления:", font=normal_font)
+    lbl_high_ad = Label(start_window, text="Для повышенного давления: ", font=normal_font)
     lbl_high_ad.place(x=40, y=200)
 
     high_ad_upper_from_label = Label(start_window, text='Верхнее (первая цифра), от:', width=normal_width)
@@ -170,28 +176,30 @@ def create_and_configure_window(normal_font, normal_width, small_width, values_d
     high_ad_upper_from_value = Entry(start_window, width=small_width)
     high_ad_upper_from_value.place(x=220, y=230)
     high_ad_upper_from_value.insert(0, '138')
-    add_value_to_dict(high_ad_upper_from_label, high_ad_upper_from_value, values_dict)
-    
-    high_ad_upper_to_label = Label(start_window, text=' до: ', width=small_width)
+    get_data_to_generate(high_ad_upper_from_value, values_dict, lbl_high_ad, high_ad_upper_from_label)
+
+    high_ad_upper_to_label = Label(start_window, text=' Верхнее, до: ', width=small_width)
     high_ad_upper_to_label.place(x=260, y=230)
     high_ad_upper_to_value = Entry(start_window, width=small_width)
     high_ad_upper_to_value.place(x=310, y=230)
     high_ad_upper_to_value.insert(0, '157')
-    add_value_to_dict(high_ad_upper_from_label, high_ad_upper_from_value, values_dict)
+    get_data_to_generate(high_ad_upper_to_value, values_dict,lbl_high_ad,high_ad_upper_to_label)
 
     high_ad_lower_from_label = Label(start_window, text='Нижнее (вторая цифра), от:', width=normal_width)
     high_ad_lower_from_label.place(x=40, y=260)
     high_ad_lower_from_value = Entry(start_window, width=small_width)
     high_ad_lower_from_value.place(x=220, y=260)
     high_ad_lower_from_value.insert(0, '85')
+    get_data_to_generate(high_ad_lower_from_value, values_dict, lbl_high_ad, high_ad_lower_from_label)
 
-    high_ad_lower_to_label = Label(start_window, text=' до: ', width=small_width)
+    high_ad_lower_to_label = Label(start_window, text=' Нижнее, до: ', width=small_width)
     high_ad_lower_to_label.place(x=260, y=260)
     high_ad_lower_to_value = Entry(start_window, width=small_width)
     high_ad_lower_to_value.place(x=310, y=260)
     high_ad_lower_to_value.insert(0, '99')
+    get_data_to_generate(high_ad_lower_to_value, values_dict, lbl_high_ad,high_ad_lower_to_label)
 
-    lbl_low_ad = Label(start_window, text="Для нормального давления:", font=normal_font)
+    lbl_low_ad = Label(start_window, text="Для нормального давления: ", font=normal_font)
     lbl_low_ad.place(x=40, y=300)
 
     low_ad_upper_from_label = Label(start_window, text='Верхнее (первая цифра), от:', width=normal_width)
@@ -199,24 +207,28 @@ def create_and_configure_window(normal_font, normal_width, small_width, values_d
     low_ad_upper_from_value = Entry(start_window, width=small_width)
     low_ad_upper_from_value.place(x=220, y=330)
     low_ad_upper_from_value.insert(0, '125')
+    get_data_to_generate(low_ad_upper_from_value, values_dict, lbl_low_ad, low_ad_upper_from_label)
 
     low_ad_upper_to_label = Label(start_window, text=' до: ', width=small_width)
     low_ad_upper_to_label.place(x=260, y=330)
     low_ad_upper_to_value = Entry(start_window, width=small_width)
     low_ad_upper_to_value.place(x=310, y=330)
     low_ad_upper_to_value.insert(0, '132')
+    get_data_to_generate(low_ad_upper_to_value, values_dict, lbl_low_ad, low_ad_upper_to_label)
 
     low_ad_lower_from_label = Label(start_window, text='Нижнее (вторая цифра), от:', width=normal_width)
     low_ad_lower_from_label.place(x=40, y=360)
     low_ad_lower_from_value = Entry(start_window, width=small_width)
     low_ad_lower_from_value.place(x=220, y=360)
     low_ad_lower_from_value.insert(0, '80')
+    get_data_to_generate(low_ad_lower_from_value, values_dict, lbl_low_ad, low_ad_lower_from_label)
 
     low_ad_lower_to_label = Label(start_window, text=' до: ', width=small_width)
     low_ad_lower_to_label.place(x=260, y=360)
     low_ad_lower_to_value = Entry(start_window, width=small_width)
     low_ad_lower_to_value.place(x=310, y=360)
     low_ad_lower_to_value.insert(0, '85')
+    get_data_to_generate(low_ad_lower_to_value, values_dict, lbl_low_ad, low_ad_lower_to_label)
 
     lbl_add = Label(start_window, text="3. Прочие параметры:", font=normal_font)
     lbl_add.place(x=40, y=400)
@@ -226,26 +238,30 @@ def create_and_configure_window(normal_font, normal_width, small_width, values_d
     soot_value = Entry(start_window, width=15)
     soot_value.place(x=220, y=430)
     soot_value.insert(0, '70')
+    get_data_to_generate(soot_value, values_dict, soot_label)
     soot_label_after = Label(start_window, text='(остальное будет нормальным)', width=normal_width)
     soot_label_after.place(x=260, y=430)
 
+
+    lbl_add_condit = Label(start_window, text="Условия заполнения", font=normal_font)
+    lbl_add_condit.place(x=40, y=490)
+
     lang = IntVar()
 
-    if_standart_checkbutton = Radiobutton(text="Простое заполнение (значения будут разбросаны в случайном порядке)",
-                                          variable=lang, value=1)
-    if_standart_checkbutton.place(x=40, y=460)
+    if_standart_checkbutton = Radiobutton(text="Простое заполнение (значения будут разбросаны в случайном порядке)", variable=lang, value=1)
+    if_standart_checkbutton.place(x=40, y=530)
 
-    if_need_night_lower_checkbutton = Radiobutton(
-        text="Вечером должно быть ниже (вечерняя пара АД будет всегда ниже, чем утренняя)", value=2, variable=lang)
-    if_need_night_lower_checkbutton.place(x=40, y=490)
+    if_need_night_lower_checkbutton = Radiobutton(text="Вечером должно быть ниже (вечерняя пара АД будет всегда ниже, чем утренняя)", value=2, variable=lang)
+    if_need_night_lower_checkbutton.place(x=40, y=570)
     if_need_night_lower_checkbutton.select()
 
-    if_need_morning_lower_checkbutton = Radiobutton(
-        text="Утром должно быть ниже (утренняя пара АД будет всегда ниже, чем вечерняя)", value=3, variable=lang)
-    if_need_morning_lower_checkbutton.place(x=40, y=520)
+    if_need_morning_lower_checkbutton = Radiobutton(text="Утром должно быть ниже (утренняя пара АД будет всегда ниже, чем вечерняя)", value=3, variable=lang)
+    if_need_morning_lower_checkbutton.place(x=40, y=610)
 
-    to_generate_btn = Button(start_window, text="Сгенерировать АД", command=run_generator)
-    to_generate_btn.place(x=40, y=560)
+    get_data_to_generate(lang, values_dict, lbl_add_condit)
+
+    to_generate_btn = Button(start_window, text="Сгенерировать АД", command=partial(run_generator, values_dict))
+    to_generate_btn.place(x=40, y=650)
 
     start_window.mainloop()
 
